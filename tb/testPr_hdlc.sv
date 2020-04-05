0d95cd1d71f2c9559daf1740df24338defcde6fa
Morten Paulsen <morten.p96@gmail.com>
2020-04-05 17:26:44 +0200

    noe

--- tb/testPr_hdlc.sv
+++ tb/testPr_hdlc.sv
@@ -150,6 +150,7 @@ program testPr_hdlc(
 	// register, and that the Rx data buffer is zero after Non-Byte-Alignment.
 	task VerifyNonByteAlignedReceive(logic [127:0][7:0] data, int Size);
 	    logic [7:0] ReadData;
+	    wait(uin_hdlc.Rx_Ready);
 
 	    // Assert that only Rx_FrameError is set
 	    ReadAddress(RXSC, ReadData);
@@ -176,7 +177,6 @@ program testPr_hdlc(
 	// register, and that the Rx data buffer is zero after frameError.
 	task VerifyFCSErrReceive(logic [127:0][7:0] data, int Size);
 	    logic [7:0] ReadData, DataLen;
-	    wait(uin_hdlc.Rx_Ready);
 
 	    // Assert that only Rx_Ready is set
 	    ReadAddress(RXSC, ReadData);
